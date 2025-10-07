e36e8966c4c2e9ba82625fd5478945d1f4dd4a08
sachinaneboinwad87-ship-it <sachinaneboinwad87@gmail.com>
2025-10-07 18:00:59 +0530

    Function in Python

---
+++ add_numbers_function.py
@@ -0,0 +1,7 @@
+# sum of two numbers using function
+def add_numbers(num1,num2):
+    return num1+num2
+result = add_numbers(33,66)
+result1 = add_numbers(num1=90,num2=13)
+print("The sum of two numbers is ",result)
+print("The sum of two number is ",result1)
---
+++ find_max.py
@@ -0,0 +1,18 @@
+# write function for find max number among 3 numbers
+
+
+
+def max_num():
+    num1 = int(input("Enter the first number :"))
+    num2 = int(input("Enter the second number : "))
+    num3 = int(input("Enter the third number :"))
+
+    if num1 > num2 and num1 > num3:
+        print("The largest number is number1", num1)
+    elif num2 > num1 and num2 > num3:
+        print("The largest number is number2 ", num2)
+    else:
+        print("The largest number is number3 ", num3)
+
+
+max_num()
